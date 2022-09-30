4bc89b8f1f410b30cbf5e970389b1c8667b0ae39
Gun woo Lee <gwlee412@gmail.com>
2022-09-30 11:06:00 -0700

    created routes to / and /search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './styles.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import SearchPage from './SearchPage';
 
 function App() {
   return (
-    <div>
-      App Placeholder
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
     </div>
   );
 }
