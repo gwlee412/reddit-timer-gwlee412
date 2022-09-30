97531a531c076c5d114bba914f63502ec7113506
Gun woo Lee <gwlee412@gmail.com>
2022-09-30 12:24:18 -0700

    Fixed Testing Error

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,1 +1,8 @@
-export default function HomePage() {}
+import { render } from '@testing-library/react';
+
+const HomePage = () => {
+  render();
+  return null;
+};
+
+export default HomePage;
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,1 +1,7 @@
-export default function HomePage() { }
+import React from 'react';
+
+export default function SearchPage() {
+  return (
+    <h1>homepage</h1>
+  );
+}
