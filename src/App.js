cfcd308a14ba5e14435de6d19d64f28b02fde4d8
Gun Woo <110861093+gwlee412@users.noreply.github.com>
2022-09-30 16:26:40 -0700

    Create app skeleton (#4)
    * installed fonts and added global classes
    
    * changed linebreak rule to match CI
    
    * Created Routes to / and /search and tested them
    
    * added react router dom
    
    * CI lint fix
    
    * added materialUI library and changed app title
    
    * MUI library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './styles.css';
+import {
+  BrowserRouter as Router,
+  useRoutes,
+} from 'react-router-dom';
+import HomePage from './HomePage';
+import SearchPage from './SearchPage';
+
+const Paths = () => {
+  const routes = useRoutes([
+    { path: '/', element: <HomePage /> },
+    { path: '/search', element: <SearchPage /> },
+  ]);
+  return routes;
+};
 
 function App() {
   return (
     <div>
-      App Placeholder
+      <Router>
+        <Paths />
+      </Router>
     </div>
   );
 }
---
+++ src/SearchPage.js
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+
+const SearchPage = () => {
+  render();
+  return null;
+};
+
+export default SearchPage;
