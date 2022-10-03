c44bfb29c497e32d42fdd775a8a0a72bcb66bc46
Gun woo Lee <gwlee412@gmail.com>
2022-10-03 11:42:35 -0700

    pixel perfect header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Container, Logo, PageLinks, NavContainer,
+} from './StyledHeader';
 
 function Header() {
   return (
-    <h2>
-      Header Placeholder
-    </h2>
+    <Container>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <NavContainer>
+        <PageLinks to="/search">
+          Search
+        </PageLinks>
+        <PageLinks to="/search">
+          How it works
+        </PageLinks>
+        <PageLinks to="/search">
+          About
+        </PageLinks>
+      </NavContainer>
+    </Container>
   );
 }
 
---
+++ src/StyledHeader.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ReactComponent } from './logo.svg';
+
+export const Container = styled.section`
+width: 100%;
+height: 100px;
+  margin: 0 auto;
+  padding: 0 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Logo = styled(ReactComponent)`
+  margin-top:8px;
+`;
+
+export const PageLinks = styled(Link)`
+  margin-left: 26px;
+  margin-top:1px;
+  text-decoration: none;
+  color: #636363;
+`;
+
+export const NavContainer = styled.section`
+display:flex;
+
+`;
