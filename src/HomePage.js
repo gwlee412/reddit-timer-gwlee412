dd0ef18ddb90824638591477c39cabdd37096b65
Gun Woo <110861093+gwlee412@users.noreply.github.com>
2022-10-04 09:17:42 -0700

    Hero section (#8)
    * installed fonts and added global classes
    
    * changed linebreak rule to match CI
    
    * Created Routes to / and /search and tested them
    
    * added react router dom
    
    * CI lint fix
    
    * Placeholder Header created
    
    * added text specific global styles
    
    * pixel perfect header
    
    * pixel perfect title with search button
    
    * merge branch 'main' into header
    
    * fixing merge
    
    * Eslint fix
    
    * Problem with react testing library solved
    
    * end to end tests
    
    * yarn build successful
    
    * footer section added
    
    * logo link test
    
    * a tag link instead for external links
    
    * heatmap image hero section
    
    * hero-section cypress tests

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,11 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import {
   TitleContainer, TitleQuestion, TitleDescription, SearchPageButton,
+  HeatMapImg, ImageContainer,
 } from './StyledHomePage';
 
 const HomePage = () => {
@@ -18,13 +20,20 @@ const HomePage = () => {
         <TitleDescription>
           Great timing, great results! Find the best time to post on your subreddit.
         </TitleDescription>
-        <SearchPageButton>
-          show me the best time
-        </SearchPageButton>
+        <Link to="/search/javascript">
+          <SearchPageButton>
+            show me the best time
+          </SearchPageButton>
+        </Link>
         <TitleDescription>
           r/javascript
         </TitleDescription>
       </TitleContainer>
+      <ImageContainer>
+        <Link to="/search/javascript">
+          <HeatMapImg />
+        </Link>
+      </ImageContainer>
       <Footer />
     </>
   );
